f5554c8dccdc74623246868a37cbc4249c0359ef
Bahaar H Sadhnani <bsadhna1@CS444-bsadhna1.pods.bu.int>
2020-05-03 19:24:07 -0400

    Partial code

--- submit/prj4-sol/blog544-ss.mjs
+++ submit/prj4-sol/blog544-ss.mjs
@@ -17,6 +17,7 @@ export default function serve(port, ws) {
   const app = express();
   app.locals.port = port;
   app.locals.ws = ws;       //web service wrapper
+app.locals.base='';
   process.chdir(__dirname); //so paths relative to this dir work
   setupTemplates(app);
   setupRoutes(app);
@@ -30,12 +31,109 @@ export default function serve(port, ws) {
 function setupRoutes(app) {
   app.use('/', express.static(STATIC_DIR));
   //@TODO add routes to handlers
+  const base=app.locals.base;
+  app.get(`/users`, listUsers(app));
+  app.get(`/search/users`, doSearch(app));
+  app.get(`${base}/search.html`, doSearch(app));
   app.use(doErrors(app)); //must be last   
 }
 
 /****************************** Handlers *******************************/
 
 //@TODO: add handlers
+
+function listUsers(app) {
+  return async function(req, res) {
+  const query=req.query;
+    const users = await app.locals.ws.list('users',query);
+    const model = { base: app.locals.base, users: users };
+    const html = doMustache(app, 'summary', model.users);
+    res.send(html);
+  };
+};
+
+
+function doSearch(app) {
+  return async function(req, res) {
+    const isSubmit = req.query.submit !== undefined;
+    let users = [];
+    let errors = undefined;
+    const search = getNonEmptyValues(req.query);
+    if (isSubmit) {
+      errors = validate(search);
+      if (Object.keys(search).length == 0) {
+  const msg = 'at least one search parameter must be specified';
+  errors = Object.assign(errors || {}, { _: msg });
+      }
+      if (!errors) {
+  const q = querystring.stringify(search);
+  try {
+    users = await app.locals.ws.list('users',q);
+  }
+  catch (err) {
+          console.error(err);
+    errors = wsErrors(err);
+  }
+  if (users.length === 0) {
+    errors = {_: 'no users found for specified criteria; please retry'};
+  }
+      }
+    }
+    let model, template;
+    if (users.length > 0) {
+      template = 'details';
+      const fields =
+  users.map((u) => ({id: u.id, fields: fieldsWithValues(u)}));
+      model = { base: app.locals.base, users: fields };
+    }
+    else {
+      template =  'search';
+      model = errorModel(app, search, errors);
+    }
+    const html = doMustache(app, template, model);
+    res.send(html);
+  };
+};
+
+function validate(values, requires=[]) {
+  const errors = {};
+  requires.forEach(function (name) {
+    if (values[name] === undefined) {
+      errors[name] =
+  `A value for '${FIELDS_INFO[name].friendlyName}' must be provided`;
+    }
+  });
+  for (const name of Object.keys(values)) {
+    const fieldInfo = FIELDS_INFO[name];
+    const value = values[name];
+    if (fieldInfo.regex && !value.match(fieldInfo.regex)) {
+      errors[name] = fieldInfo.error;
+    }
+  }
+  return Object.keys(errors).length > 0 && errors;
+}
+
+
+function getNonEmptyValues(values) {
+  const out = {};
+  Object.keys(values).forEach(function(k) {
+    if (FIELDS_INFO[k] !== undefined) {
+      const v = values[k];
+      if (v && v.trim().length > 0) out[k] = v.trim();
+    }
+  });
+  return out;
+}
+
+
+function errorModel(app, values={}, errors={}) {
+  return {
+    base: app.locals.base,
+    errors: errors._,
+    fields: fieldsWithValues(values, errors)
+  };
+}
+
 
 function doErrors(app) {
   return async function(err, req, res, next) {
@@ -47,6 +145,55 @@ function doErrors(app) {
   };
 }
 
+function wsErrors(err) {
+  const msg = (err.message) ? err.message : 'web service error';
+  console.error(msg);
+  return { _: [ msg ] };
+}
+
+
+
+/************************** Field Definitions **************************/
+
+const FIELDS_INFO = {
+  id: {
+    friendlyName: 'User Id',
+    isSearch: true,
+    isId: true,
+    isRequired: true,
+    regex: /^\w+$/,
+    error: 'User Id field can only contain alphanumerics or _',
+  },
+  firstName: {
+    friendlyName: 'First Name',
+    isSearch: true,
+    regex: /^[a-zA-Z\-\' ]+$/,
+    error: "First Name field can only contain alphabetics, -, ' or space",
+  },
+  lastName: {
+    friendlyName: 'Last Name',
+    isSearch: true,
+    regex: /^[a-zA-Z\-\' ]+$/,
+    error: "Last Name field can only contain alphabetics, -, ' or space",
+  },
+  email: {
+    friendlyName: 'Email Address',
+    isSearch: true,
+    type: 'email',
+    regex: /^[^@]+\@[^\.]+(\.[^\.]+)+$/,
+    error: 'Email Address field must be of the form "user@domain.tld"',
+  },
+  birthDate: {
+    friendlyName: 'Date of Birth',
+    isSearch: false,
+    type: 'date',
+    regex: /^\d{4}\-\d\d?\-\d\d?$/,
+    error: 'Date of Birth field must be of the form "YYYY-MM-DD"',
+  },
+};
+
+const FIELDS =
+  Object.keys(FIELDS_INFO).map((n) => Object.assign({name: n}, FIELDS_INFO[n]));
 /************************ General Utilities ****************************/
 
 /** Set up error handling for handler by wrapping it in a 
@@ -66,6 +213,17 @@ function errorWrap(handler) {
 
 function isNonEmpty(v) {
   return (v !== undefined) && v.trim().length > 0;
+}
+
+
+/** Return copy of FIELDS with values and errors injected into it. */
+function fieldsWithValues(values, errors={}) {
+  return FIELDS.map(function (info) {
+    const name = info.name;
+    const extraInfo = { value: values[name] };
+    if (errors[name]) extraInfo.errorMessage = errors[name];
+    return Object.assign(extraInfo, info);
+  });
 }
 
 /************************ Mustache Utilities ***************************/
